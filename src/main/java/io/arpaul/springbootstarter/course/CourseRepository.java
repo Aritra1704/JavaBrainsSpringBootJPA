5a49750dd4b5affa330ee8d8fed82817e7ce59a2
Aritra1704 <aritra1704@gmail.com>
2021-04-21 19:13:41 +0530

    Added Course APIs

---
+++ src/main/java/io/arpaul/springbootstarter/course/CourseRepository.java
@@ -0,0 +1,13 @@
+package io.arpaul.springbootstarter.course;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String> {
+
+	public List<Course> findByName(String name);
+	public List<Course> findByDescription(String description);
+	public List<Course> findByTopicId(String topicId);
+
+}
